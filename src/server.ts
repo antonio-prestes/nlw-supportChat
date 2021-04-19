bd1fbf8ca0a60911a1aad06f69fbfc576146e8a4
Diego <diegoantoniodasp@hotmail.com>
2021-04-19 18:06:37 -0300

    aula-01

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, {request, response} from "express";
+
+const app = express();
+
+// GET - buscas
+// POST - criação
+// PUT - alteração
+// DELETE - apagar
+// PATCH - alterar uma info especifica
+
+app.get("/",(request, response) =>{
+    return response.json({
+        message: "Opaaa"
+    })
+});
+
+app.post("/users",(request, response) =>{
+    return response.json({
+        message: "opaaa deu boa"
+    })
+})
+
+app.listen(3333, () =>console.log("rodandooooo"));
